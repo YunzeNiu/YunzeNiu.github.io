1e8bd7d6691765c901488984bf8c1b6698c80978
YunzeNiu <nnnyyyzzz731@gmail.com>
2020-10-24 22:52:00 -0400

    lab6 update

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,5 +1,11 @@
 // You may wish to find an effective randomizer function on MDN.
 
+function getRandomNum(min, max) {
+  const min1 = Math.ceil(min);
+  const max1 = Math.floor(max);
+  return Math.floor(Math.random() * (max1 - min1 + 1) + min1);
+}
+
 function range(int) {
   const arr = [];
   for (let i = 0; i < int; i += 1) {
@@ -29,8 +35,28 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
       console.log('fromServer', fromServer);
+
+      const array1 = range(10);
+      let array2 = array1.map (() => (fromServer[getRandomNum(0, 243)]));
+      while (new Set(array2).size !== array2.length) {
+        array2 = array1.map (() => (fromServer[getRandomNum(0, 243)]));
+      }
+
+      if (document.querySelector('.flex-inner')) {
+        document.querySelector('.flex-inner').remove();
+      }
+      const reversed = array2.sort((a, b) => sortFunction(b, a, 'name'));
+      const ul = document.createElement('ul');
+      ul.className = 'flex-inner';
+      $('form').prepend(ul);
+
+      reversed.forEach((el, i) => {
+        const li =document.createElement('li');
+        $(li).append(`<input type = "checkbox" id = ${el.code} value=${el.code} />`);
+        $(li).append(`<label for = ${el.code}>${el.name}</label>`);
+        $(ul).append(li);
+      });
     })
     .catch((err) => console.log(err));
 });
