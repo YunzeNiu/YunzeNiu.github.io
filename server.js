97ce9ed64e90fd34a89e1423ff117f06e724d8dd
YunzeNiu <nnnyyyzzz731@gmail.com>
2020-11-28 22:33:05 -0500

    update

--- server.js
+++ server.js
@@ -44,63 +44,74 @@ app.listen(port, () => {
 });
 
 const dbSettings = {
-  filename: './tmp/database.db',
-  driver: sqlite3.Database
-};
+	filename: './tmp/database.db',
+	driver: sqlite3.Database
+	};
 
-async function foodDataFetcher() {
-  const url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
-  const response = await fetch(url);
-  return response.json()
+async function dataFetch() {
+	const url = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
+	const response = await fetch(url);
+
+	return response.json()
+
 }
 
-async function dataInput(data) {
-  try {
-    const restaurant_name = data.name;
-    const category = data.category;
+async function insertIntoDB(data) {
+	try {
+		const restaurant_name = data.name;
+		const category = data.category;
 
-    await db.exec('INSERT INTO restaurants (restaurant_name, category) VALUES ("${restaurant_name}", "${category}")');
-    console.log(`${restaurant_name} and ${category} inserted`);
-}
+		await db.exec(`INSERT INTO restaurants (restaurant_name, category) VALUES ("${restaurant_name}", "${category}")`);
+		console.log(`${restaurant_name} and ${category} inserted`);
+		}
 
-  catch(e) {
-    console.log('Error on insertion');
-    console.log(e);
-}
+	catch(e) {
+		console.log('Error on insertion');
+		console.log(e);
+		}
+
 }
 
 async function databaseInitialize(dbSettings) {
-  try {
-    const db = await open(dbSettings);
-    await db.exec('CREATE TABLE IF NOT EXISTS restaurants (
-     id INTEGER PRIMARY KEY AUTOINCREMENT,
-     restaurant_name TEXT,
-     category TEXT)
-     ')
-    const data = await foodDataFetcher();
-    data.forEach((entry) => { dataInput(entry) });
-    const test = await db.get('SELECT * FROM restaurants')
-    console.log(test);
-}
-  catch (e) {
-    console.log('Error loading Database');
-    console.log(e);
-}
+	try {
+		const db = await open(dbSettings);
+		await db.exec(`CREATE TABLE IF NOT EXISTS restaurants (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			restaurant_name TEXT,
+			category TEXT)
+			`)
+
+		const data = await dataFetch();
+		data.forEach((entry) => { insertIntoDB(entry) });
+
+
+		const test = await db.get("SELECT * FROM restaurants")
+		console.log(test);
+
+	}
+	catch(e) {
+		console.log("Error loading Database");
+		console.log(e);
+
+	}
 }
 
-async function databaseRetriever(db) {
-  const result = await db.all('SELECT category, COUNT(restaurant_name) FROM restaurants GROUP BY category');
+async function query(db) {
+  const result = await db.all(`SELECT category, COUNT(restaurant_name) FROM restaurants GROUP BY category`);
   return result;
 }
 
 app.route('/sql')
   .get((req, res) => {
-    console.log('GET request detected');
+    console.log('GET detected');
   })
   .post(async (req, res) => {
     console.log('POST request detected');
     console.log('Form data in res.body', req.body);
-    const db = await open(dbSettings);
-    const output = await databaseRetriever(db);
+    // This is where the SQL retrieval function will be:
+    // Please remove the below variable
+		const db = await open(dbSettings);
+    const output = await query(db);
+    // This output must be converted to SQL
     res.json(output);
   });
